88f93119dbd049ddedc131cfdcf7849c39278600
Aurelia Lim <aureliadotlim@gmail.com>
2020-08-16 00:28:04 +0800

    made a loop to loop thru tracks array so i dont have to call fetch so  many times  Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 //global variables:
 const clientId ="07a76bdd80624a979c8a9b9fdb23d403";
-const clientSecret ="a415ee3805d14240a8e8d6ed2ee9bef1";
+
 let token;
+let data ;
+let danceArray = [];
+let artArray =[];
 
 
 
 var gamePage = document.getElementById('gamePage');
 var startButton = document.querySelector('.buttonGo');
 var playerPoints = 0;
-
-var song1 = {
-     track: 'Cry (with John Martin)',
+//array to store all song info
+var songs = [ 
+    { track: 'Cry (with John Martin)',
       artist: 'Gryffin, John Martin',
       id : '1TF8rXy87zrnpBlS9TLykA',
-      dancebility : 0
-};
-var song2 = {
-    track: 'Savage Love',
+      art : ""
+    
+},
+    { track: 'Savage Love',
      artist: 'Jawsh 685, Jason Derulo',
      id : '1xQ6trAsedVPCdbtDAmk0c',
-     dancebility : 0
-};
+     art :"",
+     
+},
+    { track: 'ROCKSTAR (feat. Roddy Ricch)',
+     artist: 'DaBaby, Roddy Ricch',
+     id : '7ytR5pFWmSjzHJIeQkgog4',
+     art :""
+     
+},
+{    track: 'Watermelon Sugar',
+     artist: 'Harry Styles',
+     id : '6UelLqGlWMcVH1E5c4H7lY',
+     art :""
+     
+},
+{    track: 'Dance Monkey',
+     artist: 'Tones And I',
+     id : '1rgnBhdG2JDFTbYkYRZAku',
+     art :""
+     
+},
+{    track: 'Blueberry Faygo',
+     artist: 'Lil Faygo',
+     id : '22LAwLoDA5b4AaGSkg6bKW',
+     art :""
+     
+},
+
+
+]
 
 //API PORTION
 
@@ -46,17 +77,36 @@ const _getToken = async () => {
 _getToken();
 
 // Using the token we got in order to acesss spotify's endpoints
-const getDance = async (token) => {
 
-    const result = await fetch (`https://api.spotify.com/v1/audio-features/${song1.id}`,{
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-    });
-    const data = await result.json();
-    song1.dancebility = data.dancebility;
-    return song1.dancebility;
+//get danceability info
+const getDance =  async (token) => {
+            const result = await fetch (`https://api.spotify.com/v1/audio-features/${songs[i].id}`,{
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+            });
+
+            data = await result.json();
+            let danceData = data.danceability;
+            danceArray.push(danceData)
+            return danceArray; // array will contain all the values we need! yay
+            }
+            
+ //for loop to loop thru songs array so we dont have to keep calling getdance for each array
+const getDanceAll = function () {
+    for(i = 0; i<songs.length; i++ ) {
+        getDance(token);
+        //call the art function
+}
+};
+
+// get album art
+
+const getArt = async (token) => {
+    const result = await fetch ('')
 }
 
+
+            
 
 
 
@@ -68,6 +118,7 @@ startButton.addEventListener("click", function(){
     startButton.classList.add("hide");
     console.log('game starting!')
     //fetch artist, song, album art and dancebility index from spotify
-    getDance(token);
-    console.log(song1.dancebility);
+    getDanceAll();
+    
 });
+
