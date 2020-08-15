7faadc677e861e3a89d045e23a90ba3dc6cbd583
Aurelia Lim <aureliadotlim@gmail.com>
2020-08-15 18:53:33 +0800

    styling, added two containers for images

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //global variables:
+const clientId ="07a76bdd80624a979c8a9b9fdb23d403";
+const clientSecret ="a415ee3805d14240a8e8d6ed2ee9bef1";
 
-var logo = document.getElementById('logo');
+
+
+var gamePage = document.getElementById('gamePage');
 var startButton = document.querySelector('.buttonGo');
+var playerPoints = 0;
+
 
 startButton.addEventListener("click", function(){
-    logo.classList.add("hide");
+    gamePage.classList.remove("hide");
     startButton.classList.add("hide");
     console.log('start button hidden!')
-})
+});
