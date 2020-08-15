8102a659b57a1fd4b8c266056d3fdab058fc052c
Aurelia Lim <aureliadotlim@gmail.com>
2020-08-15 21:44:38 +0800

    managed to get first step of using spotify api! the token! whooo

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ The user will have 3 seconds per round
 The user will get his result after the game.
 
 
-font-family: 'Carter One', cursive;
-font-family: 'Raleway', sans-serif;
-font-family: 'Viga', sans-serif;
+USING THE SPOTIFY API:
+ Using Client Credentials Flow: as we do NOT need user information. 
+
+ 1. Application first needs to request for authorization token
+
+url : https://accounts.spotify.com/api/token
+method: POST
+body: grant_type : client_credentials
+header: authorization: Basic <base64 encoded client_id:client_secret>
+
--- script.js
+++ script.js
@@ -8,9 +8,53 @@ var gamePage = document.getElementById('gamePage');
 var startButton = document.querySelector('.buttonGo');
 var playerPoints = 0;
 
+var song1 = {
+     track: 'Cry (with John Martin',
+      artist: 'Gryffin, John Martin',
+      id : '1TF8rXy87zrnpBlS9TLykA'
 
+}
+
+//API PORTION
+
+//get token from spotify using POST Method:
+
+const _getToken = async () => {
+
+    const result = await fetch('https://accounts.spotify.com/api/token',{
+    method: 'POST',
+    headers: {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+    },
+    body: 'grant_type=client_credentials'
+    });
+
+    const data = await result.json();
+    const token = data.access_token;
+    return token;
+    
+};
+
+// Using the token we got in order to acesss spotify's endpoints
+const _getTracks = async (token, tracksEndPoint) => {
+    const limit = 2; //cause we only want to get two tracks each turn
+
+    const result = await fetch()
+
+}
+
+
+
+
+
+
+
+//event listener for start button
 startButton.addEventListener("click", function(){
     gamePage.classList.remove("hide");
     startButton.classList.add("hide");
-    console.log('start button hidden!')
+    console.log('game starting!')
+    //fetch artist, song, album art and dancebility index from spotify
+    _getToken();
 });
